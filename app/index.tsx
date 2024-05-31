b9828197fb785487979a72a9bf383f239106b65b
Thiago Souza <thiagorasouza@gmail.com>
2024-05-30 19:33:27 -0500

    styles: assets

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View>
+        <Image source={require("@/assets/images/logo.png")} />
+      </View>
     </View>
   );
 }
